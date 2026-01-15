a2dbd2241f9afce6005a3d6f177272b5591f0a66
Shrikant Patil <pailshrikant77@gmail.com>
2026-01-16 01:01:46 +0530

    Add netlify config and fix build settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Optional: Helps if images break on Netlify
+  },
 };
 
 export default nextConfig;
